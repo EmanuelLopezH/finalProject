dbbed0c8ba9a6f60192bed71e213ec6f1a5dc3a1
Emanuel López <emanuel.lopezh@udea.edu.co>
2022-03-12 11:59:28 -0500

    Cambio en el nombre de las clases y los id

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 document.addEventListener("load", main)
 
 function main(){
-    
+    let github = document.getElementById("github")
+    github.addEventListener("click", function(){
+        alert("aaaaaaaa")
+    })
 }
